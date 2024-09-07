a5f3d5184b634386c0fd4a0e4e464b08918dc876
Kevin Mendes <keviin.nicolas18@gmail.com>
2024-09-07 18:51:02 -0300

    initial commit

---
+++ src/main/java/com/cubonetwork/cubo_back/CuboBackApplication.java
@@ -0,0 +1,13 @@
+package com.cubonetwork.cubo_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuboBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuboBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cubonetwork/cubo_back/CuboBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cubonetwork.cubo_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuboBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
